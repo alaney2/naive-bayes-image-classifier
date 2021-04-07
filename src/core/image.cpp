0a8a72a427c307a59efccb8545635ae003533d4e
Alan Yao <alanyao.training@gmail.com>
2021-04-06 20:49:46 -0500

    Added some tests.

--- src/core/image.cpp
+++ src/core/image.cpp
@@ -3,27 +3,21 @@
 namespace naivebayes {
 
 std::istream& operator>>(std::istream &is, Image &image) {
-//  std::string digit;
-//  getline(is, digit);
-//  
-//  image.class_ = std::stoi(digit);
+  std::string digit;
+  getline(is, digit);
+
+  image.class_ = std::stoi(digit);
   image.shades_.resize(kImageSize, std::vector<int>(kImageSize, 0));
   
-  for (size_t row = 0; row <= kImageSize; ++row) {
+  for (size_t row = 0; row < kImageSize; ++row) {
     std::string line;
     getline(is, line);
     
-    if (row == 0) {
-      size_t digit = std::stoi(line);
-      image.class_ = digit;
-      continue;
-    }
-    
     for (size_t col = 0; col < kImageSize; ++col) {
       if (line[col] == '+' || line[col] == '#') {
-        image.shades_[row-1][col] = 1;
+        image.shades_[row][col] = 1;
       } else {
-        image.shades_[row-1][col] = 0;
+        image.shades_[row][col] = 0;
       }
     }
   }
--- tests/model_test.cc
+++ tests/model_test.cc
@@ -14,10 +14,35 @@ TEST_CASE("Model") {
   std::string file = "/Users/alaney/CLionProjects/Cinder/my-projects/naive-bayes-alaney2/data/sample.txt";
   model.ParseFile(file);
   model.TrainModel();
-  REQUIRE(model.images_[0].GetShades()[0][0] == 0);
-  REQUIRE(model.feature_count[0][0][0][0] == 1);
-  REQUIRE(model.images_[0].GetClass() == 5);
-  model.WriteDataToFile();
+  SECTION("Correct class") {
+    REQUIRE(model.images_[0].GetClass() == 5);
+    REQUIRE(model.images_[1].GetClass() == 0);
+    REQUIRE(model.images_[3].GetClass() == 4);
+  }
+  
+  SECTION("Shaded vs unshaded") {
+    REQUIRE(model.GetImages()[0].GetShades()[0][0] == 0);
+    REQUIRE(model.GetImages()[0].GetShades()[27][27] == 0);
+    REQUIRE(model.GetImages()[0].GetShades()[11][24] == 0);
+
+    REQUIRE(model.GetImages()[0].GetShades()[5][17] == 1);
+    REQUIRE(model.GetImages()[0].GetShades()[5][18] == 1);
+    REQUIRE(model.GetImages()[0].GetShades()[6][23] == 1);
+  }
+  
+  SECTION("Feature count") {
+    REQUIRE(model.feature_count[0][0][0][0] == 1);
+    REQUIRE(model.feature_count[15][15][0][3] == 0);
+    REQUIRE(model.feature_count[3][10][0][4] == 2);
+    
+    REQUIRE(model.feature_count[15][10][1][4] == 2);
+    REQUIRE(model.feature_count[17][11][1][4] == 0);
+    REQUIRE(model.feature_count[23][19][1][4] == 1);
+  }
+  
+  SECTION("Images") {
+    REQUIRE(model.GetImages().size() == 4);
+  }
 }
 
 TEST_CASE("Probabilities") {
