7d23d10a363db9c72974fb2b1c092bd335163237
Alan Yao <alanyao.training@gmail.com>
2021-04-11 00:48:01 -0500

    Added method to calculate likelihood scores.

--- include/core/classifier.h
+++ include/core/classifier.h
@@ -1,6 +1,25 @@
 #pragma once
 
-class classify {
+#include <cstdio>
+#include <vector>
+
+#include "image.h"
+#include "model.h"
+
+namespace naivebayes {
+
+class Classifier {
+  
+ public:
+  Classifier(Model& model);
+  
+  void CalculateLikelihoodScores(Image& image);
+  
+ private:
+  Model model_;
+  std::vector<double> scores_;
   
 };
+
+}
 
--- src/core/classifier.cpp
+++ src/core/classifier.cpp
@@ -1,1 +1,23 @@
+#include "core/classifier.h"
 
+#include <core/model.h>
+
+namespace naivebayes {
+
+Classifier::Classifier(Model& model) : model_(model){ }
+
+void Classifier::CalculateLikelihoodScores(Image& image) {
+  for (size_t num = 0; num < kNumDigits; ++num) {
+    double likelihood = log(model_.GetPriorProbability(num));
+    
+    for (size_t row = 0; row < kImageSize; ++row) {
+      for (size_t col = 0; col < kImageSize; ++col) {
+        likelihood += log(image.GetShade(row, col));
+      }
+    }
+    
+    scores_[num] = likelihood;
+  }
+}
+
+}
